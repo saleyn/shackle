77bcbfe404099c15b3f7a68f7f76c313ab0a03a6
mike.taylor <mike.taylor@samsung.com>
2022-03-03 14:03:38 -0500

    Fix prometheus metric naming error (normalize pool names)

--- src/shackle_metrics.erl
+++ src/shackle_metrics.erl
@@ -1,6 +1,15 @@
 -module(shackle_metrics).
 
--export([init/1, increment/2, increment/3, timing_microseconds/3, name/1]).
+-export([init/1, increment/2, increment/3, timing_microseconds/3]).
+-export([
+    generate_counters/1,
+    generate_timing_gauges/1,
+    generate_specs/1,
+    generate_name/1,
+    generate_help/1,
+    normalize_name/1,
+    join_binaries/2
+]).
 
 -define(COUNTERS, [
     backlog_full,
@@ -35,17 +44,18 @@
 
 -spec increment(pool_name(), atom()) -> ok.
 increment(PoolName, Metric) ->
-    prometheus_counter:inc(name([PoolName, Metric, total])).
+    prometheus_counter:inc(generate_name([PoolName, Metric, total])).
 
 
 -spec increment(pool_name(), atom(), integer()) -> ok.
 increment(PoolName, Metric, Value) ->
-    prometheus_counter:inc(name([PoolName, Metric, total]), Value).
+    prometheus_counter:inc(generate_name([PoolName, Metric, total]), Value).
 
 
 -spec timing_microseconds(pool_name(), atom(), integer()) -> ok.
 timing_microseconds(PoolName, Metric, Value) ->
-    prometheus_gauge:set(name([PoolName, Metric, time, microseconds]), Value).
+    prometheus_gauge:set(generate_name([PoolName, Metric, time, microseconds]),
+                         Value).
 
 
 -spec init(pool_name()) -> ok.
@@ -67,26 +77,27 @@ generate_timing_gauges(PoolName) ->
     generate_specs([[PoolName, T, time, microseconds] || T <- ?TIMINGS]).
 
 
--spec generate_name(list(atom())) -> atom().
-generate_name(Tokens) ->
-    TokenBins = lists:map(fun erlang:atom_to_binary/1, Tokens),
-    erlang:binary_to_atom(join_binaries(TokenBins, <<"_">>)).
-
-
--spec generate_help(list(atom())) -> binary().
-generate_help(Tokens) ->
-    join_binaries(lists:map(fun erlang:atom_to_binary/1, Tokens), <<" ">>).
-
-
--spec generate_specs(list(list(atom()))) -> list(prometheus_metric_spec:spec()).
+-spec generate_specs(list(list(atom()))) ->
+    list(prometheus_metric_spec:spec()).
 generate_specs(Names) ->
     [[{name, generate_name(N)}, {help, generate_help(N)}] || N <- Names].
 
 
--spec name(list(atom())) -> ok.
-name(Parts) ->
-    Tokens = lists:map(fun erlang:atom_to_binary/1, Parts),
-    erlang:binary_to_atom(join_binaries(Tokens, <<"_">>)).
+-spec generate_name(list(atom())) -> atom().
+generate_name(Parts) ->
+    Tokens0 = lists:map(fun atom_to_binary/1, Parts),
+    Tokens1 = lists:map(fun normalize_name/1, Tokens0),
+    erlang:binary_to_atom(join_binaries(Tokens1, <<"_">>), utf8).
+
+
+-spec generate_help(list(atom())) -> binary().
+generate_help(Tokens) ->
+    join_binaries(lists:map(fun atom_to_binary/1, Tokens), <<" ">>).
+
+
+-spec normalize_name(binary()) -> binary().
+normalize_name(Name) ->
+    binary:replace(Name, <<".">>, <<"_">>, [global]).
 
 
 -spec join_binaries(BinList :: list(binary()), JoinWith :: binary()) ->
@@ -97,3 +108,8 @@ join_binaries([S], _) ->
     S;
 join_binaries([S0, S1|R], J) ->
     join_binaries([<<S0/binary, J/binary, S1/binary>>|R], J).
+
+
+-spec atom_to_binary(atom()) -> binary().
+atom_to_binary(A) ->
+    erlang:atom_to_binary(A, utf8).
